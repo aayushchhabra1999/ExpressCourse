2ce599a2006b0186c6adb7cb6121cea6ba2e4513
Aayush Chhabra <aayushgen@gmail.com>
2020-12-29 10:28:10 -0800

    styles, and home

--- movieFanSite/app.js
+++ movieFanSite/app.js
@@ -20,6 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// routes
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
